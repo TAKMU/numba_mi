a8ab6c1ae7e596afbf9bd80b8c657ecf0f399145
AMiyazono <allan.miyazono@gmail.com>
2026-06-05 11:40:28 -0600

    First commit

---
+++ src/numba_mi/pipeline.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pandas as pd
+from mi_entropy import create_mi_matrix
+from preprocess import discretize_infotheo, delete_zeroes
+import polars as pl
+from tempfile import TemporaryDirectory
+
+
+def whole_process(file_path : str, axis : int = 1, sep : str = "\t", index_col : int = 0, num_threads : int = -1 ) -> pl.DataFrame:
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+        df = delete_zeroes(file_path, axis, sep, index_col)
+        df.to_csv(f"{tmpdir}/simplified.csv")
+        discretize_infotheo(f"{tmpdir}/simplified.csv", f"{tmpdir}/discretized.csv")
+        df = pd.read_csv(f"{tmpdir}/discretized.csv")
+    return create_mi_matrix(df, num_threads = num_threads)
+
